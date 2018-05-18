2f8c5e9e69fbb9e8c4ecb2d9f6ce05dcffc183f2
ahmad nadar <ahmadnadar@gmail.com>
2018-05-18 14:09:20 +0200

    demo wims

---
+++ repo/plbank/pywims/demo.pl
@@ -0,0 +1,7 @@
+title=Demo
+
+extends=/pywims/test_pywims.pl
+
+course=3108993
+
+url=http://127.0.0.1/wims/wims.cgi?module=adm/raw&job=lightpopup&emod=classes/en&parm=cmd=new;exo=Exercise1;embededwindow=true&option=noabout
---
+++ repo/plbank/pywims/template.pl
@@ -0,0 +1,18 @@
+
+before==
+import requests
+r = requests.get("http://127.0.0.1/wims/?module=adm/raw&job=authuser&ident=myself&passwd=toto&code=ighfhg&qclass="+course+"&rclass=myclass&quser=%20qcoumes")
+session = r.json()['wims_session']
+==
+
+form==
+
+<iframe width="100%" height="600" src="{{url}}&session={{session}}">
+</iframe>
+==
+
+
+
+
+
+
