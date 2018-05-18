d6bc78222729f2742d43f97e68588ea9960c64f5
ahmad nadar <ahmadnadar@gmail.com>
2018-05-18 14:11:58 +0200

    fix host wims

--- repo/plbank/pywims/demo.pl
+++ repo/plbank/pywims/demo.pl
@@ -1,7 +1,9 @@
 title=Demo
 
-extends=/pywims/test_pywims.pl
+extends=/pywims/template.pl
 
 course=3108993
 
+host=http://127.0.0.1
 url=http://127.0.0.1/wims/wims.cgi?module=adm/raw&job=lightpopup&emod=classes/en&parm=cmd=new;exo=Exercise1;embededwindow=true&option=noabout
+
--- repo/plbank/pywims/template.pl
+++ repo/plbank/pywims/template.pl
@@ -1,7 +1,7 @@
 
 before==
 import requests
-r = requests.get("http://127.0.0.1/wims/?module=adm/raw&job=authuser&ident=myself&passwd=toto&code=ighfhg&qclass="+course+"&rclass=myclass&quser=%20qcoumes")
+r = requests.get(host+"/wims/?module=adm/raw&job=authuser&ident=myself&passwd=toto&code=ighfhg&qclass="+course+"&rclass=myclass&quser=%20qcoumes")
 session = r.json()['wims_session']
 ==
 
@@ -10,6 +10,7 @@ form==
 <iframe width="100%" height="600" src="{{url}}&session={{session}}">
 </iframe>
 ==
+
 
 
 
