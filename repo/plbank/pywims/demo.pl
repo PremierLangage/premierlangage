c93d1eaee47cd4c54e459e3d828f9ffadea49bc7
ahmad nadar <ahmadnadar@gmail.com>
2018-05-19 10:10:52 +0200

    factorisation of wims PL

--- repo/plbank/pywims/demo.pl
+++ repo/plbank/pywims/demo.pl
@@ -1,9 +1,9 @@
 title=Demo
 
 extends=/pywims/template.pl
-
+ident=myself
+quser=qcoumes
+passwd=toto
 course=3108993
-
-host=http://127.0.0.1
 url=http://127.0.0.1/wims/wims.cgi?module=adm/raw&job=lightpopup&emod=classes/en&parm=cmd=new;exo=Exercise1;embededwindow=true&option=noabout
 
