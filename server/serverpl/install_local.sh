4460004e9b3d28ee208329bd1ed213779e1d0eca
LyZeon <delver.jordan@gmail.com>
2018-06-20 10:10:49 +0200

    Update

--- server/serverpl/install_local.sh
+++ server/serverpl/install_local.sh
@@ -89,5 +89,5 @@ echo "Configuring database..."
 python3 manage.py makemigrations || { echo>&2 "ERROR: python3 manage.py makemigrations failed" ; exit 1; }
 python3 manage.py migrate || { echo>&2 "ERROR: python3 manage.py migrate failed" ; exit 1; }
 #Filling database
-python3 manage.py shell < serverpl/fill_database_local.py  || { echo>&2 "ERROR: python3 manage.py shell failed" ; exit 1; }
+python3 manage.py shell < serverpl/install/fill_database_local.py  || { echo>&2 "ERROR: python3 manage.py shell failed" ; exit 1; }
 echo "Done !"
