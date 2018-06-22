212bff0619fc7b1d9f60d46abcb8133f27f61230
LyZeon <delver.jordan@gmail.com>
2018-06-22 16:22:20 +0200

    publisher

--- server/serverpl/publisher/models.py
+++ server/serverpl/publisher/models.py
@@ -1,8 +1,9 @@
-import random
-import string
 from django.db import models
 
 
 class Publisher(models.Model):
     key = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.key +" : " + self.name
--- server/serverpl/publisher/urls.py
+++ server/serverpl/publisher/urls.py
@@ -4,6 +4,5 @@ from publisher import views
 app_name = 'publisher'
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    url(r'^publish/$', views.publish_receiver, name='publish_receiver'),
+    url(r'^publish/$', views.publish_receiver),
 ]
