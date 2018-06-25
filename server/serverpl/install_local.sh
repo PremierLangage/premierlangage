8836aaad06b3c1caa8beb27ea334960c3238f9d3
Elaad Furreedan <efurreed@etud.u-pem.fr>
2018-06-25 14:17:49 +0200

    Update install_local.sh

--- server/serverpl/install_local.sh
+++ server/serverpl/install_local.sh
@@ -77,11 +77,11 @@ echo "Done !"
 
 echo ""
 echo "Creating needed directories..."
-if [ ! -d "../../../tmp" ]; then
-    mkdir ../../../tmp || { echo>&2 "ERROR: Can't create ../../../tmp" ; exit 1; }
+if [ ! -d "../../tmp" ]; then
+    mkdir ../../tmp || { echo>&2 "ERROR: Can't create ../../tmp" ; exit 1; }
 fi
-if [ ! -f "../../../tmp/README" ]; then
-    echo "Directory used by premier langage, do not remove." > ../../../tmp/README
+if [ ! -f "../../tmp/README" ]; then
+    echo "Directory used by premier langage, do not remove." > ../../tmp/README
 fi
 
 #Building database
