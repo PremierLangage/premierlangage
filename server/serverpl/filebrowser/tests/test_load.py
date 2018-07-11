6db6617ea51bccf60c87073b4e958776287e3d77
bohraph <raphaelbohorodycz@gmail.com>
2018-07-11 11:05:53 +0200

    fix test

--- server/serverpl/filebrowser/tests/test_load.py
+++ server/serverpl/filebrowser/tests/test_load.py
@@ -61,7 +61,7 @@ class LoadTestCase(TestCase):
             self.assertEqual(response.status_code, 200)
             m = list(response.context['messages'])
             if m:
-                self.assertEqual(len(m), 1)
+                self.assertTrue(len(m) >= 1)
                 self.assertEqual(m[0].level, messages.SUCCESS)
         except AssertionError:
             m = list(response.context['messages'])
@@ -86,7 +86,7 @@ class LoadTestCase(TestCase):
             self.assertEqual(response.status_code, 200)
             m = list(response.context['messages'])
             if m:
-                self.assertEqual(len(m), 1)
+                self.assertTrue(len(m) >= 1)
                 self.assertEqual(m[0].level, messages.ERROR)
         except AssertionError:
             m = list(response.context['messages'])
--- server/serverpl/loader/parsers/pl.py
+++ server/serverpl/loader/parsers/pl.py
@@ -252,10 +252,6 @@ class Parser:
             self._multiline_key = None
             self._multiline_json = False
         else:
-            # Add warning when detecting '==' to prevent unintentionnal nested key
-            if '==' in line and self._multiline_key not in ['before', 'build', 'evaluator']:
-                self.add_warning("Nested '==' detected inside a multiple line value ("+self._multiline_key+") at line "+str(self.lineno)+". You can ignore this warning if this is intended.")
-            #print(line)
             self.add_dic2(self.dic, self._multiline_key.split("."), line, "+=")
     
     
