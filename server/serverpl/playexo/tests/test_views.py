14f86c2cb03abad24873f6fb6d9a5bbe90cbe9bb
CALLE CHRISTOPHE <ccalle@localhost.localdomain>
2018-11-27 15:14:50 +0100

    setup test_views

--- server/serverpl/playexo/tests/test_views.py
+++ server/serverpl/playexo/tests/test_views.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+
+from django.contrib.auth.models import User
+
+from playexo.views import evaluate, activity_view
 
 
 class ViewsTestCase(TestCase):
     
     @classmethod
     def setUpTestData(cls):
+        cls.factory = RequestFactory()
+        cls.user = User.objects.create_user(username='user', password='12345')
+        
+    def test_evaluate(self):
         pass
-    
-    
