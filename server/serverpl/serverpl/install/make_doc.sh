f1fa8bd5d04827c8777643d60ad6bf935d5498f4
Pavell <pavell94000@gmail.com>
2019-03-12 11:51:24 +0100

    upgraded doc

--- server/serverpl/serverpl/install/make_doc.sh
+++ server/serverpl/serverpl/install/make_doc.sh
@@ -11,3 +11,7 @@ mkdir -p documentation/static/documentation
 
 cp -r documentation/mkdocs/custom/* documentation/static/documentation/
 cp -r documentation/mkdocs/docs/images/* documentation/static/documentation/img/
+
+find documentation/templates -type f -name '*.html' -exec sed -i "s#../search/main.js#search/main.js#g" {} +
+find documentation/templates -type f -name '*.html' -exec sed -i "s#search/main.js#{% static 'documentation/search/main.js' %}#g" {} +
+find documentation/static -type f -name '*main.js' -exec sed -i "s#search/worker.js#../static/documentation/search/worker.js#g" {} +
