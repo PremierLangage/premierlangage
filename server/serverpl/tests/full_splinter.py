1ae2f7fedde49bce61d05e559454761b7ff2b6b2
Pavell <pavell94000@gmail.com>
2019-04-08 16:03:52 +0200

    updated splinter test

--- server/serverpl/tests/full_splinter.py
+++ server/serverpl/tests/full_splinter.py
@@ -1,31 +1,62 @@
-import os
+import os, shutil
 
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.test.testcases import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import override_settings
 from splinter import Browser
 
+from filebrowser.models import Directory
 
 
-class SplinterTestCase(LiveServerTestCase):
+FAKE_FB_ROOT = os.path.join(settings.BASE_DIR, 'tests/tmp')
+
+RES_DIR = os.path.join(settings.BASE_DIR, "tests/resources/fake_filebrowser_data/")
+
+
+
+@override_settings(FILEBROWSER_ROOT=FAKE_FB_ROOT)
+class SplinterTestCase(StaticLiveServerTestCase):
+    
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.u = User.objects.create_superuser("login", password="secret", email="test@test.test")
         cls.b = Browser()
-        cls.visit("filebrowser")
-        cls.b.fill("username", "login")
-        cls.b.fill("password", "secret")
-        cls.b.find_by_text("Log-in").click()
+        
+        cls.dir = Directory.objects.create(name='Yggdrasil', owner=cls.u).root
+        cls.lib = Directory.objects.create(name='lib', owner=cls.u).root
+        shutil.rmtree(os.path.join(cls.dir))
+        shutil.copytree(RES_DIR, cls.dir)
+    
     
     @classmethod
     def tearDownClass(cls):
         cls.b.quit()
+        shutil.rmtree(FAKE_FB_ROOT)
         super().tearDownClass()
+    
+    
+    def setUp(self):
+        super().setUp()
+        self.visit("filebrowser")
+        self.b.fill("username", "login")
+        self.b.fill("password", "secret")
+        self.b.find_by_text("Log-in").click()
+    
+    
+    def tearDown(self):
+        self.b.click_link_by_href("/courses/logout/")
+        super().tearDown()
     
     
     def visit(self, url):
         self.b.visit(os.path.join(self.live_server_url, url))
     
     
-    def test_login(self):
+    def test_filebrowser(self):
         self.visit("filebrowser")
+        self.assertTrue(self.b.is_text_present("home", wait_time=10))
+        self.b.find_by_text("home").click()
+        self.assertTrue(self.b.is_text_present("lib", wait_time=10))
+        self.b.find_by_text("lib").click()
