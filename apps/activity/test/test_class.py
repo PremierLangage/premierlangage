317edec4f1fbb35de7df693b553d1a0a2220712f
Pavell <pavell94000@gmail.com>
2019-06-19 18:00:06 +0200

    fixed test_class

--- apps/activity/test/test_class.py
+++ apps/activity/test/test_class.py
@@ -6,28 +6,6 @@ class JsonClassMissingKey(Exception):
     pass
 
 
-
-def class_from_json(filename):
-    with open(filename, "r") as f:
-        dic = json.loads(f.read())
-    class_name = dic["name"]
-    
-    if "name" not in dic or "attributes" not in dic or "methods" not in dic:
-        raise JsonClassMissingKey(
-            "JSON file must declare at least 'name', 'attributes' and 'methods' fields")
-    for k, v in dic["methods"].items():
-        if k in dic["attributes"]:
-            raise Exception(k + " is already a class method or attribute")
-        m = "def " + k + "(" + v["args"] + "):\n"
-        m += "\n".join([" import json
-
-
-
-class JsonClassMissingKey(Exception):
-    pass
-
-
-
 def class_from_json(filename):
     with open(filename, "r") as f:
         dic = json.loads(f.read())
@@ -56,24 +34,7 @@ def json_from_class(cls):
 
 
 if __name__ == "__main__":
-    name, new_class = class_from_json("test.json")   " + x for x in v["code"].split("\n")])
-        if "decorators" in v:
-            for deco in v["decorators"].split():
-                m = "@" + deco + "\n" + m
-        exec(m)
-        dic["attributes"][k] = eval(k)
-    return class_name, type(class_name, (object,), dic["attributes"])
-
-
-
-def json_from_class(cls):
-    pass
-
-
-
-if __name__ == "__main__":
     name, new_class = class_from_json("test.json")
-    NewClass = new_class
-    
-    a = NewClass()
+    locals()[name] = new_class
+    a = locals()[name]()
     a.printA("AAA")
