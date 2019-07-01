e05474531f9ca239c8b712281613ddf538868757
Mamadou Cisse <mciissee@gmail.com>
2019-07-01 21:03:40 +0200

    extracted editor angular project

--- apps/filebrowser/client/src/app/shared/directives/tree-node.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive } from '@angular/core';
-
-/** Element that can be used as a template for a `TreeComponent`  */
-@Directive({
-  // tslint:disable-next-line: directive-selector
-  selector: '[treeNode]'
-})
-export class TreeNodeDirective {}
