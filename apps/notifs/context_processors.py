4980ae57379e5b0b2f6d9df140fabf7c49dec9ae
Jérémie LE BASTARD <jlebas01@etud.u-pem.fr>
2019-09-09 15:37:43 +0200

    css ajusted

--- apps/notifs/context_processors.py
+++ apps/notifs/context_processors.py
@@ -3,9 +3,7 @@ from notifs.models import Notifications
 
 
 def add_notifs_processor(request):
-    if request.user.is_authenticated :
-        return {"notifications": Notifications.objects.filter(user=request.user)}
+    if request.user.is_authenticated:
+        notifs = Notifications.objects.filter(user=request.user)
+        return {"notifications": notifs, "count": len(notifs)}
     return {}
-
-
-
