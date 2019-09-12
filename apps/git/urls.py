7c60b3f117162aceed620b4bd2f9bbb41017e8ef
Pavell <pavell94000@gmail.com>
2019-09-12 17:25:15 +0200

    auto add and commit when saving file

--- apps/git/urls.py
+++ apps/git/urls.py
@@ -14,14 +14,14 @@ app_name = 'git'
 
 
 urlpatterns = [
-    path(r'add', views.add, name='add'),
-    path(r'blame', views.blame, name='blame'),
-    path(r'changes', views.changes, name='changes'),
-    path(r'checkout', views.checkout, name='checkout'),
-    path(r'clone', views.clone, name='clone'),
-    path(r'commit', views.commit, name='commit'),
-    path(r'pull', views.pull, name='pull'),
-    path(r'push', views.push, name='push'),
-    path(r'show', views.show, name='show'),
-    path(r'status', views.status, name='status'),
+    path(r'add/', views.add, name='add'),
+    path(r'blame/', views.blame, name='blame'),
+    path(r'changes/', views.changes, name='changes'),
+    path(r'checkout/', views.checkout, name='checkout'),
+    path(r'clone/', views.clone, name='clone'),
+    path(r'commit/', views.commit, name='commit'),
+    path(r'pull/', views.pull, name='pull'),
+    path(r'push/', views.push, name='push'),
+    path(r'show/', views.show, name='show'),
+    path(r'status/', views.status, name='status'),
 ]
