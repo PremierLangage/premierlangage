ea4c83efac17ae5e62f316a40a5bee37a60fe538
Pavell <pavell94000@gmail.com>
2019-09-25 14:00:03 +0200

    fixed tests

--- apps/misc_tests/test_selenium.py
+++ apps/misc_tests/test_selenium.py
@@ -171,11 +171,11 @@ class SeleniumTestCase(BaseSeleniumTestCase):
         self.visit(reverse("editor:index"))
         self.connect("login", "secret")
         e = self.b.find_element_by_id("nav-action-theme")
-        self.assertTrue(self.b.find_element_by_css_selector(".light-theme"))
-        self.assertFalse(self.b.find_elements_by_css_selector(".dark-theme"))
+        self.assertTrue(self.b.find_element_by_css_selector('body[class="light-theme"]'))
+        self.assertFalse(self.b.find_elements_by_css_selector('body[class="dark-theme"]'))
         e.click()
-        self.assertFalse(self.b.find_elements_by_css_selector(".light-theme"))
-        self.assertTrue(self.b.find_element_by_css_selector(".dark-theme"))
+        self.assertFalse(self.b.find_elements_by_css_selector('body[class="light-theme"]'))
+        self.assertTrue(self.b.find_element_by_css_selector('body[class="dark-theme"]'))
     
     
     def test_components_doc(self):
