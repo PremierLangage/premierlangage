f6791724f7e820b82655e2b55da1473095c9c085
Mamadou Cisse <mciissee@gmail.com>
2020-01-20 11:59:50 +0100

    fix(loader): allow user to update pltp generated from a gift file

--- apps/components/static/components/assets/playground/input.pl
+++ apps/components/static/components/assets/playground/input.pl
@@ -2,10 +2,18 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
-component =: Input
-component.type = number
+inputbox =: Input
+inputbox.type = number
+inputbox.placeholder = Answer
+inputbox.maxlength = 2
+inputbox.appearance = outline
 
 before==
+import random
+
+a = random.randint(1, 10)
+b = random.randint(1, 10)
+r = a + b
 ==
 
 title==
@@ -13,13 +21,17 @@ Input Component
 ==
 
 text==
+Enter the result of ** {{ a }} + {{ b }} ** inside the input box.
 ==
 
 form==
-{{ component|component}}
+{{ inputbox|component}}
 ==
 
 evaluator==
-grade = (100, component.value);
+if r == inputbox.value:
+    grade = (100, '<span class="success-state">Good 👏👏👏</span>')
+else:
+    grade = (0, '<span class="error-state">Bad answer 👎👎👎</span>')
 ==
 
