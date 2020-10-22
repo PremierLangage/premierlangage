8de2ccb64caabc880bf3d53d2f11c026452fd6fb
Christophe Callé <christophe.calle@univ-eiffel.fr>
2020-10-22 14:59:15 +0200

    duplicate activity

--- apps/activity/views.py
+++ apps/activity/views.py
@@ -28,10 +28,12 @@ def add_activity(request, activity_id):
     activity = get_object_or_404(Activity, id=activity_id)
     new_id = int(request.POST.get('new-activity-id'))
     to_add = get_object_or_404(Activity, id=new_id)
-    if to_add.activity_type == "course" or to_add.parent.id != 0 or to_add.id == 0:
+    if to_add.activity_type == "course" or to_add.id == 0:
         raise PermissionDenied("Vous ne pouvez pas ajouter cette activité")
     if not activity.is_teacher(request.user):
         raise PermissionDenied("Vous n'êtes pas professeur de cette activité")
+    if to_add.parent.id != 0:
+        to_add = to_add.duplicate()
     to_add.add_parent(activity)
     for student in activity.student.all():
         to_add.student.add(student)
