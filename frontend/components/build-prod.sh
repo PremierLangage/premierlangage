6cfa7c294e467e79995a1059f5226949a093cb83
Mamadou Cisse <mciissee@gmail.com>
2021-01-27 22:44:21 +0100

    fix: remove cdn urls

--- frontend/components/build-prod.sh
+++ frontend/components/build-prod.sh
@@ -1,13 +1,14 @@
 cat > ../../apps/components/templates/components/statics.html << ENDOFFILE
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/styles.css?env=dev"/>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/polyfills-es5.js?env=dev" nomodule defer></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/polyfills-es2015.js?env=dev" type="module"></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/scripts.js?env=dev" defer></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/runtime-es2015.js?env=dev" type="module"></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/main-es2015.js?env=dev" type="module"></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/runtime-es5.js?env=dev" nomodule defer></script>
-<script src="https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/main-es5.js?env=dev" nomodule defer></script>
+
+<link rel="stylesheet" href="{{ static('/components/styles.css') }}"/>
+<script src="{{ static('/components/polyfills-es5.js') }}" nomodule defer></script>
+<script src="{{ static('/components/polyfills-es2015.js') }}" type="module"></script>
+<script src="{{ static('/components/runtime-es2015.js') }}" type="module"></script>
+<script src="{{ static('/components/main-es2015.js') }}" type="module"></script>
+<script src="{{ static('/components/runtime-es5.js') }}" nomodule defer></script>
+<script src="{{ static('/components/main-es5.js') }}" nomodule defer></script>
+
 ENDOFFILE
 sudo npm run build:prod
--- frontend/components/src/environments/environment.prod.ts
+++ frontend/components/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  assets: 'https://cdn.staticaly.com/gh/PremierLangage/components/master/dist/assets',
-  baseUrl: 'https://cdn.staticaly.com/gh/PremierLangage/components/master/dist'
+  assets: '/static/components/assets',
+  baseUrl: '/static/components'
 };
