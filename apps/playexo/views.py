44a2ca80c091f46cb30a12cb47dfca6beeb75f05
yfleche <yfleche@etud.u-pem.fr>
2021-06-24 14:19:09 +0200

    feat: :rocket: minimizing the null activities

--- apps/playexo/views.py
+++ apps/playexo/views.py
@@ -1,8 +1,10 @@
+from apps.activity.activity_type.course import Course
 import io
 import json
 import logging
 import traceback
 from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 
 import htmlprint
 from activity.models import Activity
@@ -21,24 +23,22 @@ from user_profile.models import Profile
 from re import split, template
 from json import dumps
 from django.http import HttpResponse
-
+import time
 
 logger = logging.getLogger(__name__)
 
-activitiesIds = Answer.objects.values_list("activity_id",flat=True)
-activities = Activity.objects.values_list(
-    "name", "id").filter(id__in = activitiesIds).order_by('id')
+#creation of the basic list to print in download_answers.html 
 
-sizeMaxActivity = max(map(lambda activity: len(activity[0]), activities))//1.3
+activities = Activity.objects.values_list("name", "id").order_by('id')
+sizeMaxActivity = max( map( lambda activity: len(activity[0]), activities))//1.3
 
 
-plsIds = Answer.objects.all().values_list("pl_id")
-pls = PL.objects.all().values_list("name", "id").filter(id__in = plsIds).order_by('id')
+pls = PL.objects.all().values_list("name", "id").order_by('id')
 sizeMaxPl = max(map(lambda pl: len(pl[0]), pls))//1.3
 
 
 courses = Activity.objects.all().values_list("name", "id").filter(activity_type="course").order_by('id')
-sizeMaxCourse = max(map(lambda course: len(course[0]), courses))//1.3
+sizeMaxCourse = max( map( lambda course: len(course[0]), courses))//1.3
 
 tags = set()
 tags.update(['tag1', 'tag2', 'tag3', 'tag4'])
@@ -51,13 +51,19 @@ students = list(map(lambda student: (student[0], split(
 teachers = list(map(lambda teacher: (teacher[0], split(
     "[/_]", teacher[1])[1]), Profile.objects.filter(role=2).values_list("user_id", "avatar")))
 
-def find_course_name(parents, id: int, name: str):
+def find_course_name(parents: dict, id: int, name: str):
+    """
+        This function return the name of the course at the root of the activity_id: id
+    """
     while id != 0:
-        tmp = parents[id]
-        id,name = tmp[0], tmp[1]
+        id,name = parents[id][0], parents[id][1]
     return name
 
-def filter_by_login_or_role(login: bool, request, sql_request: Q ):
+def filter_by_login_or_role(login: bool, request: HttpRequest, sql_request: Q ):
+    """
+        This function return a new request which will filter our final request
+        with a login in particular, or a role (student/teacher) 
+    """
     if login :
         try:
             if request.GET["studentLogin"].isnumeric():
@@ -67,9 +73,10 @@ def filter_by_login_or_role(login: bool, request, sql_request: Q ):
             sql_request &= Q(user_id = name)
             
         except (ValueError,Profile.DoesNotExist) as err :
-            raise HttpResponseNotFound("User does not exist")
+            raise err
         
     elif "type" in request.GET:
+        tmp=[]
         if request.GET["type"] == "teacher":
             tmp = list(map(lambda teacher: teacher[0], teachers))
         elif request.GET["type"] == "students":
@@ -77,7 +84,11 @@ def filter_by_login_or_role(login: bool, request, sql_request: Q ):
         sql_request &= Q(user_id__in = tmp)
     return sql_request
 
-def filter_by_grade(minInRequest: bool, maxInRequest: bool, request, sql_request: Q):
+def filter_by_grade(minInRequest: bool, maxInRequest: bool, request: HttpRequest, sql_request: Q):
+    """
+        This function will filter the final result by the grade, the grade must be inferior/superior or between
+        request.GET['max']/request.GET['min'] or the both
+    """
     if maxInRequest or minInRequest:
         if not maxInRequest:
             if minInRequest:
@@ -94,7 +105,11 @@ def filter_by_grade(minInRequest: bool, maxInRequest: bool, request, sql_request
     # rajouter si Q(grade = None) ? 
     return sql_request 
 
-def filter_by_date(startInRequest: bool, endInRequest: bool, request, sql_request: Q):
+def filter_by_date(startInRequest: bool, endInRequest: bool, request:HttpRequest, sql_request: Q):
+    """
+        This function will filter the final result by the date, it must be inferior/superior or between
+        request.GET['end']/request.GET['start'] or the both
+    """
     if not startInRequest:
         if endInRequest :
             sql_request &= Q(date__lte = request.GET["end"])
@@ -105,6 +120,45 @@ def filter_by_date(startInRequest: bool, endInRequest: bool, request, sql_reques
         sql_request &= Q(date__range = (request.GET["start"], request.GET["end"]))
     return sql_request
 
+def filter_by_pl(plInRequest: bool, request: HttpRequest, sql_request: Q):
+    """
+        This function return a new request which will filter our final request
+        with a pl in particular
+    """
+    if plInRequest:
+            try:
+                try:
+                    pl = int(request.GET['pl'])
+                except ValueError:
+                    pl = int(request.GET['pl2'])
+                sql_request &= Q(pl = PL.objects.get(id = pl))
+            except (PL.DoesNotExist, ValueError) as err:
+                raise err
+                return HttpResponseNotFound("PL does not exist")
+    return sql_request
+
+def filter_by_activity(activityInRequest: bool, request: HttpRequest, sql_request: Q):
+    if activityInRequest:
+        try:
+            try:
+                activity = int(request.GET['activity'])
+            except ValueError:
+                activity = int(request.GET['activity2'])
+            sql_request &= Q(activity = Activity.objects.get(id = activity))
+
+        except (Activity.DoesNotExist, ValueError) as err:
+            raise err
+                # return HttpResponseNotFound("Activity does not exist")
+    return sql_request
+
+def filter_by_course(courseInRequest:bool, request: HttpRequest, sql_request: Q):
+    if courseInRequest:
+        try:
+            sql_request &= Q(activity__in = Activity.objects.filter(
+                parent = int(request.GET["course"])).values_list("id", flat=True))
+        except (Activity.DoesNotExist, ValueError) as err:
+            raise err
+    return sql_request
 
 @login_required
 def test_pl(request, pl_id):
@@ -176,62 +230,63 @@ def test_pl(request, pl_id):
 
 @login_required
 @require_GET
-def download_answers(request):
+def download_answers(request: HttpRequest):
+    """
+        This function will parse the request to download all the informations
+        necessary to get the JSON required
+    """
     if not request.user.is_staff:
         raise PermissionDenied
     if "start" in request.GET or "end" in request.GET:
-        sql_request = Q()
+        sql_request = Q() #creation of an emtpy request that we will feed gradually
                 
         answers = Answer.objects.select_related("activity", "pl", "user")
 
-        startInRequest = "start" in request.GET and request.GET["start"]!=''
+        # the differents boolean follow down, help us to know if elements
+        # are present in the request and to calculate if
+        # they're required in the final result or not  
+        
+        startInRequest = "start" in request.GET and request.GET["start"]!='' 
         endInRequest = "end" in request.GET and request.GET["end"]!=''
 
         plInRequest = "pl" in request.GET and request.GET['pl'].isnumeric() or "pl2" in request.GET and request.GET['pl2'].isnumeric()
         activityInRequest = "activity" in request.GET and request.GET['activity'].isnumeric() or "activity2" in request.GET and request.GET['activity2'].isnumeric()
+        courseInRequest = "course" in request.GET and request.GET["course"].isnumeric()
 
         maxInRequest = "max" in request.GET and request.GET["max"].isnumeric()
         minInRequest = "min" in request.GET and request.GET["min"].isnumeric()
 
-        actifInRequest = "actif" in request.GET
+        actifInRequest = "actif" in request.GET and request.GET["actif"] == "on" 
 
-        limit = "limit" in request.GET
+        limit = "limit" in request.GET and request.GET["limit"].isnumeric()
         login = "teacherLogin" in request.GET and request.GET["teacherLogin"].isnumeric()  or "studentLogin" in request.GET and request.GET["studentLogin"].isnumeric()
 
 
+        #the lines below are adding the potential filters to sql_request
 
         sql_request = filter_by_date(startInRequest, endInRequest, request, sql_request)
         
-        if plInRequest:
-            try:
-                try:
-                    pl = int(request.GET['pl'])
-                except ValueError:
-                    pl = int(request.GET['pl2'])
-                sql_request &= Q(pl = PL.objects.get(id = pl))
-            except (PL.DoesNotExist, ValueError) as err:
-                return HttpResponseNotFound("PL does not exist")
+        try:
+            sql_request = filter_by_pl(plInRequest, request, sql_request)
+        except (ValueError,PL.DoesNotExist):
+            return HttpResponseNotFound("PL does not exist")
 
-        if activityInRequest:
-            try:
-                try:
-                    activity = int(request.GET['activity'])
-                except ValueError:
-                    activity = int(request.GET['activity2'])
-                sql_request &= Q(activity = Activity.objects.get(id = activity))
+        try:
+            sql_request = filter_by_activity(plInRequest, request, sql_request)
+        except (ValueError,Activity.DoesNotExist):
+            return HttpResponseNotFound("Activity does not exist")
 
-            except (Activity.DoesNotExist, ValueError) as err:
-                return HttpResponseNotFound("Activity does not exist")
+        try:
+            sql_request = filter_by_course(plInRequest, request, sql_request)
+        except (Activity.DoesNotExist, ValueError):
+            return HttpResponseNotFound("Course does not exist")
 
-        if "course" in request.GET and request.GET["course"].isnumeric():
-            try:
-                sql_request &= Q(activity__in = Activity.objects.filter(
-                    parent = int(request.GET["course"])).values_list("id", flat=True))
-            except (Activity.DoesNotExist, ValueError) as err:
-                return HttpResponseNotFound("Course does not exist")
-                
-        sql_request = filter_by_login_or_role(login, request, sql_request)
+
         
+        try:
+            sql_request = filter_by_login_or_role(login, request, sql_request)
+        except (ValueError,Profile.DoesNotExist):
+            return HttpResponseNotFound("User does not exist")
         
                 
 
@@ -243,40 +298,79 @@ def download_answers(request):
 
         sql_request = filter_by_grade(minInRequest, maxInRequest, request, sql_request)
         
-        if actifInRequest and request.GET["actif"] == "on":
+        if actifInRequest :
             sql_request &= Q(activity_id__in = Activity.objects.select_related("id").all().filter(
                 open=True).values_list("id", flat=True))
 
-        answers = answers.filter(sql_request)
+        answers = answers.filter(sql_request) # application of the differents filter
 
         if answers.count() == 0:
             return HttpResponseBadRequest("There is no informations in our database linked to your request", status=400)
             
-        if limit and request.GET["limit"].isnumeric():
+        if limit :
             answers = answers[:int(request.GET["limit"])]
 
+        # creation of a dictionnary which will have the key 'id' 
+        # equal to the answer's id, and an other dictionnary in value
+        # that will stock the informations about the user and the exercise 
+        # that he submits
+        dic = dict()
         slice_size = 1_000
-        dic = {
-            answer.id: {
-                "user": answer.user.get_username(),
-                "seed": answer.seed,
-                "date": str(answer.date),
-                "grade": answer.grade,
-                "pl_id": answer.pl.id,
-                "pl_name": answer.pl.name,
-                "activit_id" : answer.activity.id  if answer.activity is not None else None,
-                "activity_name" : answer.activity.name if answer.activity is not None else None,
-                "open" : answer.activity.open if answer.activity is not None else None,
-                "activity_name" : answer.activity.name if answer.activity is not None else None,
-                "include_answers": answer.answers if "include_answers" in request.GET else None,
-                "tag": answer.pl.json["tag"].split("|") if "include_tag" in request.GET and "tag" in answer.pl.json else None,
-                "cours": find_course_name(parents, answer.activity.parent_id, answer.activity.name ) if answer.activity is not None else None
-            } 
 
-            for i in range(0, answers.count(), slice_size)
-                for answer in answers[i: i + slice_size]  
-        }
+        #just here to test the perfomance of the dictionnary's cretation
         
+        # start_time = time.time()
+
+
+        for i in range(0, answers.count(), slice_size):
+                for answer in answers[i: i + slice_size]:
+                    dic[answer.id] = {
+                        "user": answer.user.get_username(),
+                        "seed": answer.seed,
+                        "date": str(answer.date),
+                        "grade": answer.grade,
+                        "pl_id": answer.pl.id,
+                        "pl_name": answer.pl.name,
+                        "include_answers": answer.answers if "include_answers" in request.GET else None,
+                        "enseignement": PL.objects.all().values_list("rel_path", flat=True).get(id = answer.pl.id).split('/')[0],
+                        "tag": answer.pl.json["tag"].split("|") if "include_tag" in request.GET and "tag" in answer.pl.json else None,
+                    }
+                    
+                    try:
+                        if answer.activity is None:
+                            answer.activity = Activity.objects.get(pl = answer.pl.id)
+                    except Activity.DoesNotExist:
+                        for value in ["activity_id", "activity_name", "open", "cours"]:
+                            dic[answer.id][value] = None
+                    else:
+                        dic[answer.id]["activity_id"] = answer.activity.id  
+                        dic[answer.id]["activity_name"] = answer.activity.name 
+                        dic[answer.id]["open"] = answer.activity.open 
+                        dic[answer.id]["cours"] = find_course_name(parents, answer.activity.parent_id, answer.activity.name )
+
+        
+        # dic = {
+        #     answer.id: {
+        #         "user": answer.user.get_username(),
+        #         "seed": answer.seed,
+        #         "date": str(answer.date),
+        #         "grade": answer.grade,
+        #         "pl_id": answer.pl.id,
+        #         "pl_name": answer.pl.name,
+        #         "activity_id" : answer.activity.id  if answer.activity is not None else Activity.objects.get(pl = answer.pl.id).name if answer is not None else None,
+        #         "activity_name" : answer.activity.name if answer.activity is not None else None, #print( Activity.objects.get(pl = answer.pl.id).name),
+        #         "open" : answer.activity.open if answer.activity is not None else None, #Activity.objects.get(pl = answer.pl.id).open,
+        #         "include_answers": answer.answers if "include_answers" in request.GET else None,
+        #         "enseignement": PL.objects.all().values_list("rel_path",flat=True).get(id = answer.pl.id).split('/')[0],
+        #         "tag": answer.pl.json["tag"].split("|") if "include_tag" in request.GET and "tag" in answer.pl.json else None,
+        #         "cours": find_course_name(parents, answer.activity.parent_id, answer.activity.name ) if answer.activity is not None else None
+        #             # else find_course_name(parents, Activity.objects.get(pl = answer.pl.id).parent_id, Activity.objects.get(pl = answer.pl.id).name )
+        #     } 
+        #     for i in range(0, answers.count(), slice_size)
+        #         for answer in answers[i: i + slice_size]
+        # }
+
+        # print("--- %s seconds ---" % (time.time() - start_time))
 
         stream = io.StringIO(json.dumps(dic))
         response = StreamingHttpResponse(stream, content_type="application/json")
