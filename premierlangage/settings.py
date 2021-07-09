a739005dea2243886929b5dbb39563e37906b43a
yfleche <yfleche@etud.u-pem.fr>
2021-07-09 10:45:17 +0200

    fix: :bug: middleware bug

--- premierlangage/settings.py
+++ premierlangage/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS += ('django_jinja','django_extensions')
 # Middleware definition
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
