2ccd31c423f2ec341e5436add6a7db752514a650
YohanRameau <yohan.rameau@gmail.com>
2021-09-03 13:59:39 +0200

    fix bug

--- apps/lti_app/admin.py
+++ apps/lti_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import ActivityOutcome, LTIModel
+from .models import ActivityOutcome
 
-admin.site.register(LTIModel)
 admin.site.register(ActivityOutcome)
