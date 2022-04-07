9a1d9542f9f9acb3f4c092cb1af97cfe9cc4dbdc
yohan_rameau <yohan.rameau@gmail.com>
2022-04-07 16:00:15 +0200

    feat(fill PLATON with data): add some users with a script

--- script/fill_database_local.py
+++ script/fill_database_local.py
@@ -20,6 +20,106 @@ try:
 except django.db.utils.IntegrityError:
     print("User 'admin' already created")
     user = User.objects.get(username='admin')
+    
+    
+# Create some users
+try:
+    user = User.objects.create_user(username='test_user_1', password='password')
+    user.is_staff = True
+    user.is_admin = True
+    user.is_superuser = True
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_1' already created")
+    user = User.objects.get(username='admin')
+    
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_2', password='password')
+    user.is_staff = True
+    user.is_admin = True
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_2' already created")
+    user = User.objects.get(username='admin')
+
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_3', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_3' already created")
+    user = User.objects.get(username='admin')
+    
+    
+try:
+    user = User.objects.create_user(username='test_user_4', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_4' already created")
+    user = User.objects.get(username='admin')
+    
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_5', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_5' already created")
+    user = User.objects.get(username='admin')
+
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_6', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_6' already created")
+    user = User.objects.get(username='admin')
+    
+try:
+    user = User.objects.create_user(username='test_user_7', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_7' already created")
+    user = User.objects.get(username='admin')
+    
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_8', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_8' already created")
+    user = User.objects.get(username='admin')
+
+# Create admin
+try:
+    user = User.objects.create_user(username='test_user_9', password='password')
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+except django.db.utils.IntegrityError:
+    print("User 'test_user_9' already created")
+    user = User.objects.get(username='admin')
+
 
 # Create anonymous
 try:
