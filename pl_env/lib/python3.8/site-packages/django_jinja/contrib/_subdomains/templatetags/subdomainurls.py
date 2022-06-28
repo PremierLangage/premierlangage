c28303b54b088bf818e4617777f105f4b16f5627
clementg <derkeethus96@gmail.com>
2022-06-28 10:53:45 +0200

    removed env

--- pl_env/lib/python3.8/site-packages/django_jinja/contrib/_subdomains/templatetags/subdomainurls.py
+++
@@ -1,9 +0,0 @@
-from django_jinja import library
-from jinja2 import contextfunction
-from subdomains.templatetags.subdomainurls import url as subdomain_url
-
-
-@library.global_function
-@contextfunction
-def url(context, *args, **kwargs):
-    return subdomain_url(context, *args, **kwargs)
