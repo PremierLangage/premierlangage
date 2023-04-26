c42d396338cb36c6a0f684f45041cb32e8364752
osmocode <96887741+osmocode@users.noreply.github.com>
2023-04-26 15:10:14 +0200

    feat: preview feature eval grid 💫

--- frontend/components/src/assets/playground/eval-grid.pl
+++ frontend/components/src/assets/playground/eval-grid.pl
@@ -4,8 +4,33 @@ builder =@ /builder/before.py
 
 grid =: EvalGrid
 
+
 # GENERATE A RANDOM QUESTION
 before==
+
+grid.items = [
+    {
+        'title': 'First step of evaluation grid',
+        'description': 'For default check choice set "selected"',
+        'choices': [
+            'Choice with index 0',
+            'Choice with index 1',
+            'Choice with index 2',
+            'Choice with index 3',
+        ],
+        'selected': 1
+    },
+    {
+        'title': 'Second step of evaluation grid',
+        'description': 'This feature is preview feature',
+        'choices': [
+            'Choice with index 0',
+            'Choice with index 1',
+            'Choice with index 2',
+            'Choice with index 3',
+        ]
+    },
+]
 
 ==
 
@@ -22,5 +47,16 @@ form==
 
 # EVALUATE THE STUDENT ANSWER
 evaluator==
+
+feedback = ""
+
+for item in grid.items:
+    if item['selected'] == -1:
+        feedback += "Undefined"
+    else:
+        feedback += item['choices'][item['selected']]
+    feedback += "<br>"
+
+grade = (100, feedback)
 
 ==
