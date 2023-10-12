bfb4728e8496ce4605495c952cc8a4d9652c4a60
Thomas Saillard <thomas.saillard@protonmail.com>
2023-10-12 13:41:49 +0200

    feat: adding dependencies inside the projet

---
+++ common_static/dependencies/p-181b7c2a.js
@@ -0,0 +1,4 @@
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+import{i as o}from"./p-76378400.js";const s=o;export{s as g}
---
+++ common_static/dependencies/p-eac263dd.entry.js
@@ -0,0 +1,4 @@
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+import{r as o,h as s,H as r}from"./p-06ac429a.js";import{c as t}from"./p-1d072d3d.js";import{b as a}from"./p-76378400.js";const c=class{constructor(s){o(this,s),this.color=void 0}render(){const o=a(this);return s(r,{class:t(this.color,{[o]:!0})},s("slot",null))}};c.style=":host(.ion-color){color:var(--ion-color-base)}";export{c as ion_text}
