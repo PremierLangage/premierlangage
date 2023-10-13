ad28ef6b0de40198fe80780f3aacf4ad0346411f
Thomas Saillard <thomas.saillard@protonmail.com>
2023-10-13 13:42:58 +0200

    more logging

--- apps/cas_app/backends.py
+++ apps/cas_app/backends.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django_cas_ng.backends import CASBackend
 from django.contrib import messages
 from django.conf import settings
@@ -10,8 +11,24 @@ class MyCASBackend(CASBackend):
         return True
 
     def bad_attributes_reject(self, request, username, attributes):
-        logger.info("--------------------------- bad_attributes_reject --------------------------------")
-        logger.info(attributes)
-        logger.info(username)
-        logger.info(request)
+        logger.debug("--------------------------- bad_attributes_reject --------------------------------")
+        logger.debug(attributes)
+        logger.debug(username)
+        logger.debug(request)
         return None
+    
+    def authenticate(self, request, ticket, service):
+        logger.debug("--------------------------- authenticate --------------------------------")
+        logger.debug(ticket)
+        logger.debug(service)
+        logger.debug(request)
+        user = super().authenticate(request, ticket, service)
+        logger.debug(user)
+        if user:
+            logger.debug("User is authenticated")
+        return user
+    
+
+    def configure_user(self, user: User) -> User:
+        logger.debug("--------------------------- configure_user --------------------------------")
+        return super().configure_user(user)
