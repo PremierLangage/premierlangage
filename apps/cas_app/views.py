90b8f18cdaadd7a793532ba3392331416491af05
Thomas Saillard <thomas.saillard@protonmail.com>
2023-10-13 12:59:21 +0200

    adding logs

---
+++ apps/cas_app/views.py
@@ -0,0 +1,24 @@
+from django_cas_ng import views as baseviews
+from django.views.decorators.csrf import csrf_exempt
+import logging 
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
+def login(request, **kwargs):
+    return log_waths_happening(request, baseviews.LoginView.as_view()(request, **kwargs))
+
+
+def logout(request, **kwargs):
+    return log_waths_happening(request, baseviews.LoginView.as_view()(request, **kwargs))
+
+
+def log_waths_happening(request, response):
+    """If the given HttpResponse is a redirect to CAS, then add the proper
+    `locale` parameter to it (and return the modified response). If not, simply
+    return the original response."""
+
+    logger.error("log_waths_happening")
+    logger.error(response)
+    logger.error(request)
+    return response
