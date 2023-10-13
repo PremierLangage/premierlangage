e2dd110f5f09ce7cdd120e90cbfc51b4451d276c
Thomas Saillard <thomas.saillard@protonmail.com>
2023-10-13 12:59:21 +0200

    adding logs

--- apps/cas_app/urls.py
+++ apps/cas_app/urls.py
@@ -11,14 +11,14 @@
 #
 
 from django.urls import path
+from cas_app import views 
 from django_cas_ng import views as cas_views
-
 
 
 app_name = 'cas_app'
 
 urlpatterns = [
-    path('login', cas_views.LoginView.as_view(), name='cas_ng_login'),
-    path('logout', cas_views.LogoutView.as_view(), name='cas_ng_logout'),
+    path('login', views.login, name='cas_ng_login'),
+    path('logout', views.logout, name='cas_ng_logout'),
     path('callback', cas_views.CallbackView.as_view(), name='cas_ng_proxy_callback'),
 ]
