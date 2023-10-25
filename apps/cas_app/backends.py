91df3ba04f625bd365c858aacfc0580fc0204fa3
Thomas Saillard <thomas.saillard@protonmail.com>
2023-10-25 15:31:08 +0200

    refactor: making authenticate function shorter

--- apps/cas_app/backends.py
+++ apps/cas_app/backends.py
@@ -22,16 +22,15 @@ def get_or_none(model, **kwargs):
         except model.DoesNotExist:
             return None
 
+def tp_note_register_user(next, user):
+    if next and next.startswith("/tpnote/"):
+        next = ''.join(i for i in next if i.isdigit())
+        activity_id = int(next)
+        activity = get_object_or_404(Activity, id=activity_id)
+        if activity.activity_type == "course":
+            activity.add_student_to_all(user)
+
 class MyCASBackend(CASBackend):
-
-    
-
-    def bad_attributes_reject(self, request, username, attributes):
-        logger.debug("--------------------------- bad_attributes_reject --------------------------------")
-        logger.debug(attributes)
-        logger.debug(username)
-        logger.debug(request)
-        return None
     
     def authenticate(self, request : HttpRequest, ticket : str, service : str):
         """
@@ -39,22 +38,14 @@ class MyCASBackend(CASBackend):
 
         :returns: [User] Authenticated User object or None if authenticate failed.
         """
-        logger.debug("--------------------------- authenticate --------------------------------")
-        logger.debug(ticket)
-        logger.debug(service)
-        logger.debug(request)
 
         next = request.GET.get('next', None)
 
         client = get_cas_client(service_url=service, request=request)
         username, attributes, pgtiou = client.verify_ticket(ticket)
 
-        logger.debug("ticket verified")
         if attributes and request:
             request.session['attributes'] = attributes
-
-        logger.debug("attributes: ", attributes)
-        logger.debug("request: ", request)
         
         if settings.CAS_USERNAME_ATTRIBUTE != 'cas:user' and settings.CAS_VERSION != 'CAS_2_SAML_1_0':
             if attributes:
@@ -62,8 +53,6 @@ class MyCASBackend(CASBackend):
             else:
                 return None
 
-        logger.debug("not CAS_2_SAML_1_0")
-        logger.debug("username: ", username)
         if not username:
             return None
         user = None
@@ -74,8 +63,6 @@ class MyCASBackend(CASBackend):
             if reject:
                 return None
 
-            # If we can, we rename the attributes as described in the settings file
-            # Existing attributes will be overwritten
             for cas_attr_name, req_attr_name in settings.CAS_RENAME_ATTRIBUTES.items():
                 if cas_attr_name in attributes and cas_attr_name is not req_attr_name:
                     attributes[req_attr_name] = attributes[cas_attr_name]
@@ -83,9 +70,6 @@ class MyCASBackend(CASBackend):
 
         UserModel = get_user_model()
 
-        # Note that this could be accomplished in one try-except clause, but
-        # instead we use get_or_create when creating unknown users since it has
-        # built-in safeguards for multiple threads.
         if settings.CAS_CREATE_USER:
             user_kwargs = {
                 UserModel.USERNAME_FIELD: username
@@ -99,43 +83,25 @@ class MyCASBackend(CASBackend):
         else:
             created = False
             try:
-                if settings.CAS_LOCAL_NAME_FIELD:
+                user_kwargs = {
+                    UserModel.EMAIL_FIELD: username + "@univ-eiffel.fr"
+                }
+                user = get_or_none(UserModel, **user_kwargs)
+                if not user: 
                     user_kwargs = {
-                        settings.CAS_LOCAL_NAME_FIELD: username
-
+                        UserModel.EMAIL_FIELD: username + "@edu.univ-eiffel.fr"
                     }
                     user = UserModel._default_manager.get(**user_kwargs)
-                else:
-                    user_kwargs = {
-                        UserModel.EMAIL_FIELD: username + "@univ-eiffel.fr"
-                    }
-                    user = get_or_none(UserModel, **user_kwargs)
-                    if not user: 
-                        user_kwargs = {
-                            UserModel.EMAIL_FIELD: username + "@edu.univ-eiffel.fr"
-                        }
-                        user = UserModel._default_manager.get(**user_kwargs)
-
             except UserModel.DoesNotExist:
                 pass
 
         if not self.user_can_authenticate(user):
             return None
         
-        logger.debug("going through authenticate 0")
-
         if pgtiou and settings.CAS_PROXY_CALLBACK and request:
             request.session['pgtiou'] = pgtiou
         
-        logger.debug("going through authenticate 1")
-
         if settings.CAS_APPLY_ATTRIBUTES_TO_USER and attributes:
-            # If we are receiving None for any values which cannot be NULL
-            # in the User model, set them to an empty string instead.
-            # Possibly it would be desirable to let these throw an error
-            # and push the responsibility to the CAS provider or remove
-            # them from the dictionary entirely instead. Handling these
-            # is a little ambiguous.
             user_model_fields = UserModel._meta.fields
             for field in user_model_fields:
                 # Handle null -> '' conversions mentioned above
@@ -154,31 +120,12 @@ class MyCASBackend(CASBackend):
                         continue
 
             user.__dict__.update(attributes)
-
-            # If we are keeping a local copy of the user model we
-            # should save these attributes which have a corresponding
-            # instance in the DB.
             if settings.CAS_CREATE_USER:
                 user.save()
-        
-        logger.debug("going through authenticate 2")
 
-        if next and next.startswith("/tpnote/"):
-            # remove all non digit characters from next
-            next = ''.join(i for i in next if i.isdigit())
-            activity_id = int(next)
-            activity = get_object_or_404(Activity, id=activity_id)
-            if activity.activity_type == "course":
-                activity.add_student_to_all(user)  # add student to all groups
+        # TP note feature 
+        tp_note_register_user(next, user)
 
-        logger.debug("going through authenticate 3")
-        # request.user = user
-        # auth.login(request, user)
-
-        logger.debug("going through authenticate 4")
-
-        if user:
-            logger.debug(f"User ({user}) is authenticated")
         return user
     
 
